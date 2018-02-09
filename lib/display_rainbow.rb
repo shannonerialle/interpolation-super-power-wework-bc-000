fe0e5ee05ec9aba13f9137d89135d26604ab4c1f
Shannon Ford <shannon.ford@wework.com>
2018-02-09 00:53:36 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,9 @@
 # Write your #display_rainbow method here
 def display_rainbow (x)
   puts "{x[0]}"
+  puts "{x[0]}"
+  puts "{x[0]}"
+  puts "{x[0]}"
+  puts "{x[0]}"
+  puts "{x[0]}"
+  puts "{x[0]}"
